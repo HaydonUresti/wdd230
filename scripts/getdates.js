81996f66d3c15aa13ddbfb376396427fa3281f64
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2024-04-25 00:38:06 -0600

    Added the home page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the last modified date in the footer
+document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+// Set the copyright date in the footer
+document.querySelector('#currentYear').innerHTML = `©${new Date().getFullYear()} <br> Haydon Uresti <br> Idaho `;
