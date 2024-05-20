eff3fd309cffafd6ebf7e5768afb58936c74d027
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2024-05-20 14:39:13 -0600

    Created the foundation for the chamber home page

--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -1,2 +1,4 @@
+// Set the copyright date in the footer
+document.querySelector('#currentYear').innerHTML = `${new Date().getFullYear()}`;
 // Set the last modified date in the footer
 document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
