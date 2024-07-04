09275ff8e7715cc97a1209959af7128ff5493232
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2024-07-04 15:58:30 -0600

    Fixded the directory page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,7 +4,7 @@ const cards = document.querySelector('.card-mode');
 
 document.querySelector("#toggle-button").addEventListener("click", () => {
     const data = document.querySelector('#members');
-    data.classList.toggle("cards-mode");
+    data.classList.toggle("card-mode");
     data.classList.toggle("list-mode");
 
 })
@@ -18,10 +18,10 @@ const displayCards = (members) => {
             <h2>${member.name}</h2>
             <img src="${member.imageURL}" alt="${member.name} image" loading="lazy" height="200" width="">
             <p><strong>${member.membership}</strong></p>
-            <p>${member.address}</p>
-            <p>${member.address2}</p>
+            <p>${member.address}<br> ${member.address2}</p>
+            
             <p>${member.phone}</p>
-            <p>${member.website}</p>
+            <a href="${member.website}">Website</a>
         `
         cards.append(newCard);
         newCard.classList.add("dir-card")
