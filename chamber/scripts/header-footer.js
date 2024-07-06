9fc0902c432c788b38eabd531b82f97ea57d8414
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2024-07-06 14:34:50 -0600

    Fixed theme button

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -18,21 +18,21 @@ let darkbutton = document.getElementById("theme-button");
 darkbutton.addEventListener("click", () => {
     darkbutton.classList.toggle("dark")
     if (darkbutton.classList.contains("dark")) {
-        document.documentElement.style.setProperty('--text-color', 'black');
-        document.documentElement.style.setProperty('--body-color', 'white');
-        document.documentElement.style.setProperty('--card-color', '#db995a');
-        document.documentElement.style.setProperty('--border-color', '#82A0BC');
-        document.documentElement.style.setProperty('--header-color', '#828E9B');
-        document.documentElement.style.setProperty('--link-highlight-color', 'azure');
-    }
-
-    else {
         document.documentElement.style.setProperty('--text-color', 'white');
         document.documentElement.style.setProperty('--body-color', 'black');
         document.documentElement.style.setProperty('--card-color', '#9e6a3a');
         document.documentElement.style.setProperty('--border-color', '#526476');
         document.documentElement.style.setProperty('--header-color', '#4c5a69');
         document.documentElement.style.setProperty('--link-highlight-color', 'black');
+    }
+
+    else {
+        document.documentElement.style.setProperty('--text-color', 'black');
+        document.documentElement.style.setProperty('--body-color', 'white');
+        document.documentElement.style.setProperty('--card-color', '#db995a');
+        document.documentElement.style.setProperty('--border-color', '#82A0BC');
+        document.documentElement.style.setProperty('--header-color', '#828E9B');
+        document.documentElement.style.setProperty('--link-highlight-color', 'azure');
     }
 
 })
