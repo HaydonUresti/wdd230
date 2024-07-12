3473aa383a5593163ca78357995a52bc0bc1dac8
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2024-07-11 20:02:25 -0600

    Started and made progress on the header

---
+++ store/scripts/feader-footer.js
@@ -0,0 +1,14 @@
+// Last updated and date info
+// Set the copyright date in the footer
+// document.querySelector('#currentYear').innerHTML = `${new Date().getFullYear()}`;
+// Set the last modified date in the footer
+// document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+
+// The hamburger menu
+let menu_button = document.getElementById("menu-button");
+let navlist = document.querySelector("nav ul");
+menu_button.addEventListener('click', () => {
+    menu_button.classList.toggle("open")
+    navlist.classList.toggle("open")
+})
