b0c5a695ed7d4fd2fcf8d51b15c76aef23d2f8a0
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2024-07-18 22:20:41 -0600

    Added to the Order page

--- store/scripts/products.js
+++ store/scripts/products.js
@@ -2,6 +2,9 @@ const productsSection = document.getElementById("products-section");
 const path = './data/products.json'
 
 const populateProduts = (products) => {
+    // this value will be passed to the button as the value attribute to give to the Order page.
+    let productCounter = 0;
+
     products.forEach((product) => {
         let newCard = document.createElement("div");
         newCard.innerHTML = `
@@ -10,11 +13,14 @@ const populateProduts = (products) => {
             <img src="${product.ProductImageURL}" alt="${product.name} image" loading="lazy" height="200" width="">
             <p>${product.ProductDescription}</p>
             <p><strong>$${product.ProductPrice}</strong></p>
+            <a href="order.html?product=${product.ProductName}">
             <button id="order-button">Order Now</button
-        `
+            </a>
+            `
         productsSection.append(newCard);
         newCard.classList.add("card")
         newCard.classList.add("product-card")
+        productCounter += 1;
 
 
     });
@@ -25,7 +31,6 @@ async function getProductData() {
     const response = await fetch(path);
 
     if (response.ok) {
-        console.log("got here")
         const data = await response.json();
         populateProduts(data.products);
     }
