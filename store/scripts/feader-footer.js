0a897bcf0b7418f68dbdf2a8190f17592539cf5a
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2024-07-19 20:40:23 -0600

    minor fixes

--- store/scripts/feader-footer.js
+++ store/scripts/feader-footer.js
@@ -1,6 +1,7 @@
 // Last updated and date info
 // Set the copyright date in the footer
-document.querySelector('#currentYear').innerHTML = `${new Date().getFullYear()}`;
+
+document.getElementById('currentYear').innerText = `${new Date().getFullYear()}`;
 // Set the last modified date in the footer
 document.querySelector("#last-modified").innerHTML = `Last Modified: ${document.lastModified}`;
 
